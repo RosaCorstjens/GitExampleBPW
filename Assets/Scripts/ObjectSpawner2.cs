cbbe8f8bfeac8d746476846615b3742cf0980493
Mikey W <mikewoudstra888@gmail.com>
2020-02-27 12:28:51 +0100

    XD

--- Assets/Scripts/ObjectSpawner2.cs
+++ Assets/Scripts/ObjectSpawner2.cs
@@ -1,33 +1,38 @@
-﻿    using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSpawner : MonoBehaviour {
+public class ObjectSpawner2 : MonoBehaviour
+{
     public int maxObjects = 100;
     public Vector3 area;
     private GameObject[] objects;
     private List<GameObject> spawnedObjects = new List<GameObject>();
 
-    private void Start() {
-        objects = Resources.LoadAll<GameObject>("Objects");
+    private void Start()
+    {
+        objects = Resources.LoadAll<GameObject>("Objects2");
         StartCoroutine(SpawnObjects());
     }
 
-    IEnumerator SpawnObjects() {
+    IEnumerator SpawnObjects()
+    {
         int numObjects = 0;
-        while (numObjects < maxObjects) {
+        while (numObjects < maxObjects)
+        {
             yield return null;
             Vector3 position = new Vector3(
                 Random.Range(0.5f * -area.x, 0.5f * area.x),
-                0,
-                Random.Range(0.5f * -area.z, 0.5f * area.z)); 
+                Random.Range(0.5f * -area.y, 0.5f * area.y),
+                Random.Range(0.5f * -area.z, 0.5f * area.z));
             Quaternion rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
             spawnedObjects.Add(Instantiate(objects[Random.Range(0, objects.Length)], position, rotation));
             numObjects++;
         }
     }
 
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(Vector3.zero, area);
     }
