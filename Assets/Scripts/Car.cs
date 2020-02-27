afeef3708c5ee7e3d53c3fde72de062633b07ca7
narasu <narasu@users.noreply.github.com>
2020-02-27 12:47:59 +0100

    added car

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.right*speed*Time.deltaTime);
+    }
+}
