1bdc0098834308e36643f046af24b5a5b812784a
narasu <narasu@users.noreply.github.com>
2020-02-27 12:53:23 +0100

    Added random rotation to car

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -8,7 +8,7 @@ public class Car : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.rotation = Quaternion.Euler(0,Random.Range(0,360),0);
     }
 
     // Update is called once per frame
